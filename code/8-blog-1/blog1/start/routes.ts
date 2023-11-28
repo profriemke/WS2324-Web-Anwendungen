ae3375cd7d1c280e515be7897917d84127af0486
prof_riemke <riemke-gurzki@hdm-stuttgart.de>
2023-11-28 15:53:01 +0100

    LV2 28.11.23

---
+++ code/8-blog-1/blog1/start/routes.ts
@@ -0,0 +1,57 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer''
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+Route.get('/', async ({ view }) => {
+  const posts = await Database.from('posts')
+                              .select('*')
+ return view.render('index',{ posts })
+})
+Route.post('/admin/post/create', async ({ request, response }) => {
+  try{
+  const result = await Database.table('posts')
+                         .insert({
+                          title: request.input('title'),
+                          teaser: request.input('teaser'),
+                          text: request.input('text'),
+                          author: request.input('author'),
+                          date: new Date()
+                         })
+  }catch(err){
+    console.log(err);
+    return 'error'+err;
+  }
+  return response.redirect('/');
+});
+
+Route.get('/admin/post/create', async ({ view }) => {
+  return view.render('admin_post_create')
+
+});
+
+Route.get('/kunden', async ({ view }) => {
+  const kunden = await Database.from('kunde')
+                               .select('kundennr', 'vorname', 'nachname')
+                               .where('plz', 72658)
+                               .limit(10);
+  return view.render('kunden', { kunden })
+});
